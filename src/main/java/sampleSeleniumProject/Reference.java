0084cbed03721307998b04a716c2b0e45af2150d
Vikram <vikramgundkar@gmail.com>
2022-07-20 19:56:54 +0530

    First commit

---
+++ src/main/java/sampleSeleniumProject/Reference.java
@@ -0,0 +1,16 @@
+package sampleSeleniumProject;
+
+public class Reference {
+
+	public static void main(String[] args) {
+		
+		int a=2;
+		int b=6;
+		int c=a+b;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
